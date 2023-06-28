80855c80961493528863d671e246b9ad46d0633d
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-06-28 17:03:02 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/router'
+import Navber from './Navber/Navber'
 
 function App() {
   
 
   return (
     <div>
+      <Navber/>
       <RouterProvider router={router} />
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   )
 }
---
+++ src/Navber/Navber.jsx
@@ -0,0 +1,28 @@
+
+
+const Navber = () => {
+    return (
+        
+            <nav>
+                <div>
+                    <div>
+                        <h1>logo</h1>
+                    </div>
+                    <div>
+
+
+                        <ul>
+                            <li>Home</li>
+                            <li>Service</li>
+                            <li>Production</li>
+                            <li>Blog</li>
+                            <li>About</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        
+    );
+};
+
+export default Navber;
